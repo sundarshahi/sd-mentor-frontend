6f05a19e6ccdbaaae9a9766dc611c135ffa6614b
tishamaharjan <139973058+tishamaharjan@users.noreply.github.com>
2025-05-07 17:27:04 +0545

    fix(progressBar): created components instead of working in app.js

---
+++ progressBar/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import ProgressBar from "./components/progressbar";
+
+function App() {
+  return(
+    <ProgressBar />
+  )
+}
+
+export default App;
---
+++ progressBar/src/components/progressbar.jsx
@@ -0,0 +1,58 @@
+import styles from "./ProgressBar.module.css";
+import React, { useState } from "react";
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+
+  const AddProgress = () => {
+    if (progress >= 100) {
+      setProgress(100);
+    } else {
+      setProgress(progress + 10);
+    }
+  };
+
+  const DecreaseProgress = () => {
+    if (progress <= 0) {
+      setProgress(0);
+    } else {
+      setProgress(progress - 10);
+    }
+  };
+
+  const progressColor = () => {
+    if (progress >= 80) {
+      return "green";
+    } else if (progress >= 40 && progress < 80) {
+      return "orange";
+    } else {
+      return "red";
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Progress Bar</h1>
+      <div className={styles.progressBar}>
+        <div
+          style={{
+            height: "100%",
+            width: `calc(${progress} * 2px)`,
+            backgroundColor: progressColor(),
+          }}
+        >
+          <div style={{ textAlign: "center" }}>{progress}%</div>
+        </div>
+      </div>
+      <div>
+        <button className={styles.btn} onClick={DecreaseProgress}>
+          -10%
+        </button>
+        <button className={styles.btn} onClick={AddProgress}>
+          +10%
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
