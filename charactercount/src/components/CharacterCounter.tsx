c76232a49550cd89106a52e311f03116b219c0d3
tishamaharjan <139973058+tishamaharjan@users.noreply.github.com>
2025-05-16 12:17:00 +0545

    fix(charactercoun): using tailwincss

--- charactercount/src/components/CharacterCounter.tsx
+++ charactercount/src/components/CharacterCounter.tsx
@@ -1,4 +1,3 @@
-import styles from "./CharacterCounter.module.css";
 import { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "../App";
 
@@ -85,7 +84,7 @@ const CharacterCounter = () => {
 
   const handleSetCharacterLimit = (e: React.ChangeEvent<HTMLInputElement>) => {
     const isChecked = e.target.checked;
-    console.log(isChecked,"checked");
+    console.log(isChecked, "checked");
 
     setIsCharLimitActive(isChecked);
     if (!isChecked) {
@@ -98,35 +97,35 @@ const CharacterCounter = () => {
   };
 
   return (
-    <div className={styles.main}>
-      <div className={styles.topContainer}>
+    <div className="flex flex-col items-center content-center w-full max-sm:p-2.5 max-md:p-2.5">
+      <div className="flex items-center w-3/4 max-sm:w-full max-md:w-full mt-[10px]">
         <div>Character Counter</div>
-        <div className={styles.icon}>
+        <div className="bg-gray-300 h-[33px] w-[33px] rounded-lg ml-[auto]">
           <img
             src={theme === "light" ? moon : sun}
             alt="Theme"
             onClick={switchTheme}
-            className={styles.img}
+            className="h-[30px] w-[30px] m-[2px] cursor-pointer"
           />
         </div>
       </div>
-      <h1 style={{ justifyContent: "center", display: "flex" }}>
+      <h1 className="flex content-center mt-5 text-3xl mb-2.5 w-1/2">
         Analyze your text in real-time
       </h1>
 
-      <div className={styles.container}>
+      <div className="flex flex-col w-3/4 max-sm:w-full max-md:w-full max-sm:h-[fit-content]">
         <textarea
           onChange={handleTextChange}
           placeholder="Start typing here..."
           value={text}
-          className={styles.textAreaField}
+          className="rounded-md h-[100px] min-w-[150px] p-2.5 bg-[var(--text-field-background)] text-[var(--field-text)]"
         />
-        <div className={styles.inputDiv}>
-          <label className={styles.label}>
+        <div className="w-full flex mt-2.5 text-[14px] max-sm:flex-col">
+          <label className="mr-1.5 flex content-center text-gray-500">
             <input type="checkbox" onChange={handleExcludeSpacesChange} />
             Exclude Spaces
           </label>
-          <label className={styles.label}>
+          <label className="mr-1.5 flex content-center text-gray-500">
             <input type="checkbox" onChange={handleSetCharacterLimit} />
             Set Character Limit
           </label>
@@ -135,47 +134,61 @@ const CharacterCounter = () => {
               <input
                 type="number"
                 min="1"
-                className={styles.limitInput}
+                className="w-[55px] max-sm:[200px] h-4 border border-black mr-0.5 p-1 rounded"
                 value={characterlimit > 0 ? characterlimit : ""}
                 onChange={(e) => setCharacterLimit(Number(e.target.value))}
                 placeholder="Enter character limit"
               />
-              <button onClick={applyLimit} className={styles.applyButton}>Apply</button>
+              <button
+                onClick={applyLimit}
+                className="ml-1 mr-1 text-[10px] border border-black p-0.5 rounded"
+              >
+                Apply
+              </button>
             </div>
           )}
-          <div className={styles.readingTime}>
+          <div className="ml-auto max-sm:ml-0 text-gray-500">
             Approx.reading time: {readingTime} minute
           </div>
         </div>
 
-        <div className={styles.countContainer}>
-          <div className={styles.lettercount}>
-            <div className={styles.p}>{characterCount}</div>Total Characters
+        <div className="mt-7.5 flex max-sm:flex-col gap-4 max-md:gap[1rem] w-full max-md:w-full text-black">
+          <div className="rounded-2xl w-[32%] max-sm:w-full  p-1.5 min-w-[200px] max-md:min-w-[100px] height-[110px] bg-[url(https://images.pexels.com/photos/7130870/pexels-photo-7130870.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load)]">
+            <div className="text-[40px] font-bold mt-[7px]">
+              {characterCount}
+            </div>
+            Total Characters
           </div>
-          <div className={styles.wordscount}>
-            <div className={styles.p}>{wordscount}</div>Word Count
+          <div className="rounded-2xl w-[32%] max-sm:w-full p-1.5 min-w-[200px] max-md:min-w-[100px] height-[110px] bg-[url(https://images.pexels.com/photos/7135052/pexels-photo-7135052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)]">
+            <div className="text-[40px] font-bold mt-[7px]">{wordscount}</div>
+            Word Count
           </div>
-          <div className={styles.sentencecount}>
-            <div className={styles.p}>{sentencecount}</div>Sentence Count
+          <div className="rounded-2xl w-[32%] max-sm:w-full p-1.5 min-w-[200px] max-md:min-w-[100px] height-[110px] bg-[url(https://images.pexels.com/photos/7130873/pexels-photo-7130873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)]">
+            <div className="text-[40px] font-bold mt-[7px]">
+              {sentencecount}
+            </div>
+            Sentence Count
           </div>
         </div>
       </div>
 
-      <div className={styles.bottomContainer}>
+      <div className="flex w-[75%] max-sm:w-full max-md:w-full flex-col mt-3">
         <h3>Letter Density</h3>
         <div>
-          {" "}
           {Object.entries(letterDensity).length > 0 && (
-            <ul className={styles.ul}>
+            <ul className="list-none m-0 p-0">
               {(showAll
                 ? Object.entries(letterDensity).sort((a, b) => b[1] - a[1])
                 : Object.entries(letterDensity)
                     .sort((a, b) => b[1] - a[1])
                     .slice(0, 5)
               ).map(([letter, percent]) => (
-                <li key={letter} className={styles.li}>
+                <li
+                  key={letter}
+                  className="flex items-center mb-2.5 text-gray-500"
+                >
                   <span style={{ width: "5%" }}>{letter.toUpperCase()}</span>
-                  <div className={styles.progressBar}>
+                  <div className="w-[78%] max-sm:w-[68%] h-2.5 rounded-[5px] mr-auto bg-(--background-pogress)">
                     <div
                       style={{
                         width: `${percent}%`,
@@ -186,7 +199,7 @@ const CharacterCounter = () => {
                     ></div>
                   </div>
 
-                  <div style={{ color: "grey" }}>
+                  <div className="text-gary-500">
                     {letterCount[letter]}
                     {" ("}
                     {percent}%{")"}
@@ -196,7 +209,10 @@ const CharacterCounter = () => {
             </ul>
           )}
           {Object.entries(letterDensity).length > 5 && (
-            <button onClick={toggleShowAll} className={styles.morelessBtn}>
+            <button
+              onClick={toggleShowAll}
+              className="w-fit bg-transparent border border-none cursor-pointer text-gray-400"
+            >
               {showAll ? "Show Less \u02c4" : "Show More \u02c5"}
             </button>
           )}
